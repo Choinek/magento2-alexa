2381547172cb76168e38b48f0703b0b894c6dd30
Choinek <adrian@chojnicki.pl>
2018-09-05 19:05:04 +0200

    change sample intent schema, add more reports

--- app/code/Global4Net/AlexaReport/App/AlexaReportApp.php
+++ app/code/Global4Net/AlexaReport/App/AlexaReportApp.php
@@ -12,6 +12,7 @@ use Magento\Customer\Model\ResourceModel\Visitor\CollectionFactory;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Customer\Model\Visitor;
 use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Store\Model\StoreManagerInterface;
 
 
 /**
@@ -28,7 +29,7 @@ class AlexaReportApp extends AlexaApplicationAbstract implements AlexaApplicatio
     /**
      * @var Visitor
      */
-    private $visitorModel;
+    protected $visitorModel;
 
     /**
      * @var DateTime
@@ -36,10 +37,16 @@ class AlexaReportApp extends AlexaApplicationAbstract implements AlexaApplicatio
     protected $date;
 
     /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
      * Constructor.
      * @param ResponseDataFactory $responseDataFactory
      * @param CollectionFactory $customerVisitorCollection
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param StoreManagerInterface $storeManager
      * @param Visitor $visitorModel
      * @param DateTime $date
      */
@@ -47,11 +54,12 @@ class AlexaReportApp extends AlexaApplicationAbstract implements AlexaApplicatio
         ResponseDataFactory $responseDataFactory,
         CollectionFactory $customerVisitorCollection,
         SearchCriteriaBuilder $searchCriteriaBuilder,
+        StoreManagerInterface $storeManager,
         Visitor $visitorModel,
         DateTime $date
-
     )
     {
+        $this->storeManager = $storeManager;
         $this->responseDataFactory = $responseDataFactory;
         $this->customerVisitorCollection = $customerVisitorCollection;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
@@ -69,6 +77,10 @@ class AlexaReportApp extends AlexaApplicationAbstract implements AlexaApplicatio
     {
         if ($intentName == 'OnlineCustomersReport') {
             return $this->onlineCustomersReport($slots);
+        } elseif ($intentName == 'LoadingTimeReport') {
+            return $this->loadingTimeReport();
+        } elseif ($intentName == 'SystemReport') {
+            return $this->systemReport();
         }
 
         $response = $this->responseDataFactory->create();
@@ -86,31 +98,146 @@ class AlexaReportApp extends AlexaApplicationAbstract implements AlexaApplicatio
         $response = $this->responseDataFactory->create();
 
         if (isset($slots['minutes']) && $slots['minutes'] > 0) {
-            $minutesInterval = $slots['minutes'];
-            $text = 'In the last ' . (int)$slots['minutes'] . ' minutes there were %s people online';
+            $interval = $slots['minutes'] * 60;
+            $timeIntervalText = $slots['minutes'] . ' minutes';
         } elseif (isset($slots['hours']) && $slots['hours'] > 0) {
-            $minutesInterval = $slots['hours'] * 60;
-            $text = 'In the last ' . (int)$slots['hours'] . ' hours there were %s people online';
+            $interval = $slots['hours'] * 60 * 60;
+            $timeIntervalText = $slots['hours'] . ' hours';
         } else {
-            $minutesInterval = $this->visitorModel->getOnlineInterval();
-            $text = 'In the last ' . (int)$minutesInterval . ' minutes there were %s people online';
+            $interval = $this->visitorModel->getOnlineInterval() * 60;
+            $timeIntervalText = $this->visitorModel->getOnlineInterval() . ' minutes';
         }
 
         $collection = $this->customerVisitorCollection->create();
 
         $dateTime = new \DateTime();
-        $dateTime->setTimestamp($this->date->gmtTimestamp() - ($minutesInterval * 60));
+        $dateTime->setTimestamp($this->date->gmtTimestamp() - $interval);
 
         $collection->addFieldToFilter('last_visit_at', array('gteq' => $dateTime->format('Y-m-d H:i:s')));
 
         $customersOnlineCount = $collection->getSize();
 
-        $text = $customersOnlineCount ? sprintf($text, $customersOnlineCount) : sprint($text, 'no');
+        $text = 'In the last %s there were %s people online';
+
+        $text = $customersOnlineCount
+            ? sprintf($text, $timeIntervalText, $customersOnlineCount)
+            : sprintf($text, $timeIntervalText, 'no');
 
         $response->setResponseText($text);
-        $response->setCardSimple("Customers online", $text);
+        $response->setCardSimple("Report - Customers Online", $text);
         $response->setShouldEndSession(true);
 
         return $response;
+    }
+
+    /**
+     * @return ResponseData
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function loadingTimeReport()
+    {
+        $response = $this->responseDataFactory->create();
+        $baseUrl = $this->storeManager->getStore()->getBaseUrl();
+
+        $start = microtime(true);
+        $pageContent = file_get_contents($baseUrl);
+        $measuredTime = microtime(true) - $start;
+
+        $text = '';
+        $textAdd = '';
+
+        if (strpos($pageContent, 'Magento_Ui/js/core/app') !== false) {
+            $text .= sprintf(
+                'Homepage load time was <emphasis level="strong">%s</emphasis>',
+                $this->timePronunciation($measuredTime)
+            );
+
+            if ($measuredTime < 0.3) {
+                $textAdd .= ' <amazon:effect name="whispered"> It\'s good, your shop is fast... </amazon:effect>';
+            }
+        } else {
+            $text .= sprintf(
+                'Warning, homepage might be corrupted. Anyway load time was: %s',
+                $this->timePronunciation($measuredTime)
+            );
+        }
+
+        $response->setResponseSsml('<speak>' . $text . $textAdd . '</speak>');
+        $response->setCardSimple("Report - Loading Time", $text);
+        $response->setShouldEndSession(true);
+
+        return $response;
+    }
+
+    /**
+     * @return ResponseData
+     */
+    public function systemReport()
+    {
+        $response = $this->responseDataFactory->create();
+
+        $text = '<speak>';
+        if ($this->isLinux()) {
+            $cpuUsage = sys_getloadavg();
+            $text .= ' Processor load: <emphasis level="strong">' . $cpuUsage[0] . '</emphasis>';
+            $text .= '<break time="500ms"/>';
+            $text .= ' Available memory: <emphasis level="strong">'
+                . $this->getServerAvailableMemory() . '</emphasis> percent.';
+        } else {
+            $text = 'System report is currently supported only in Linux environment.';
+        }
+        $text .= '</speak>';
+
+        $response->setResponseSsml($text);
+        $response->setCardSimple("Report - System", $text);
+        $response->setShouldEndSession(true);
+
+        return $response;
+    }
+
+    /**
+     * Check if current process is running under linux
+     * @todo I didn't think about for too long, so it might be false sometimes. :)
+     *
+     * @return bool
+     */
+    public function isLinux()
+    {
+        return (strtolower(substr(PHP_OS, 0, 3)) === 'lin');
+    }
+
+    /**
+     * Get percentage information about available memory
+     * @return float|int
+     */
+    public function getServerAvailableMemory()
+    {
+        $free = shell_exec('free');
+        $free = (string)trim($free);
+        $freeArray = explode("\n", $free);
+        $mem = explode(" ", $freeArray[1]);
+        $mem = array_filter($mem);
+        $mem = array_merge($mem);
+        $memoryAvailablePercent = $mem[6] / $mem[1];
+
+        return round($memoryAvailablePercent * 100);
+    }
+
+    /**
+     * This method represent seconds / microseconds in understandable form
+     *
+     * @param $time time in seconds
+     * @return string
+     */
+    public function timePronunciation($time)
+    {
+        if ($time < 1) {
+            return sprintf('%s microseconds', round($time * 1000));
+        } else {
+            $seconds = floor($time);
+            $microseconds = round($time - $seconds * 1000);
+
+            return sprintf('%s seconds and %s microseconds', $seconds, $microseconds);
+        }
     }
 }
